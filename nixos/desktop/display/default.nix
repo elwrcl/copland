5ce676cfd16b83cbc13363d0ddb68ae890f5d627
elwrcl <elwerici@icloud.com>
2026-04-04 15:08:01 +0300

    add: ly config

--- nixos/desktop/display/default.nix
+++ nixos/desktop/display/default.nix
@@ -2,8 +2,16 @@
 
 {
   services.xserver.enable = true;
-  services.displayManager.ly.enable = true;
-
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "doom";
+      margin_box_h = 2;
+      margin_box_v = 1;
+      hide_borders = false;
+      bigclock = true;
+    };
+  };
   services.xserver.xkb = {
     layout = "tr";
     variant = "";
