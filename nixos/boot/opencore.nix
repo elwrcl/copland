05e666f3693a87fb4c92c4411f2b350048fc835e
elwrcl <elwerici@icloud.com>
2026-04-15 01:28:30 +0300

    fix2

--- nixos/boot/opencore.nix
+++ nixos/boot/opencore.nix
@@ -19,36 +19,38 @@ let
     inherit pkgs;
     modules = [
       ({ ... }: {
-        config.UEFI.Output.ProvideConsoleGop = true;
-        config.UEFI.Output.Resolution = "1366x768";
-        config.UEFI.Output.TextRenderer = "BuiltinGraphics";
-        config.Misc.Boot.PickerMode = "External";
-        config.Misc.Boot.PickerVariant = "Acidanthera\\Syrah";
-        config.Misc.Boot.PickerAttributes = 17;
-        config.Misc.Boot.Timeout = 10;
-        config.Misc.Security.SecureBootModel = "Disabled";
-        config.Misc.Security.ScanPolicy = 0;
-        config.UEFI.Quirks.DisableSecurityPolicy = true;
-        config.UEFI.Quirks.ReleaseUsbOwnership = true;
-        config.PlatformInfo.Automatic = true;
-        config.PlatformInfo.Generic.SystemProductName = "MacBookPro16,1";
-        config.PlatformInfo.Generic.SystemSerialNumber = "C02DF0Y0MD6N";
-        config.PlatformInfo.Generic.SystemUUID = "5445524E-A59B-4D8B-9B2F-9876543210AB";
+        oceanix = {
+          UEFI.Output.ProvideConsoleGop = true;
+          UEFI.Output.Resolution = "1366x768";
+          UEFI.Output.TextRenderer = "BuiltinGraphics";
+          Misc.Boot.PickerMode = "External";
+          Misc.Boot.PickerVariant = "Acidanthera\\Syrah";
+          Misc.Boot.PickerAttributes = 17;
+          Misc.Boot.Timeout = 10;
+          Misc.Security.SecureBootModel = "Disabled";
+          Misc.Security.ScanPolicy = 0;
+          UEFI.Quirks.DisableSecurityPolicy = true;
+          UEFI.Quirks.ReleaseUsbOwnership = true;
+          PlatformInfo.Automatic = true;
+          PlatformInfo.Generic.SystemProductName = "MacBookPro16,1";
+          PlatformInfo.Generic.SystemSerialNumber = "C02DF0Y0MD6N";
+          PlatformInfo.Generic.SystemUUID = "5445524E-A59B-4D8B-9B2F-9876543210AB";
 
-        config.Misc.Entries = [
-          {
-            Name = "Limine";
-            Enabled = true;
-            Path = "PciRoot(0x0)/Pci(0x1f,0x2)/Sata(2,0,0)/HD(1,GPT,73ae5e9a-c31d-4787-9792-ee1843de3e9d,0x800,0x200000)/\\EFI\\limine\\BOOTX64.EFI";
-          }
-        ];
+          Misc.Entries = [
+            {
+              Name = "Limine";
+              Enabled = true;
+              Path = "PciRoot(0x0)/Pci(0x1f,0x2)/Sata(2,0,0)/HD(1,GPT,73ae5e9a-c31d-4787-9792-ee1843de3e9d,0x800,0x200000)/\\EFI\\limine\\BOOTX64.EFI";
+            }
+          ];
 
-        config.UEFI.Drivers = [
-          { Path = "OpenRuntime.efi"; Enabled = true; }
-          { Path = "OpenCanopy.efi"; Enabled = true; }
-          { Path = "OpenLinuxBoot.efi"; Enabled = true; }
-          { Path = "OpenUsbKbDxe.efi"; Enabled = true; }
-        ];
+          UEFI.Drivers = [
+            { Path = "OpenRuntime.efi"; Enabled = true; }
+            { Path = "OpenCanopy.efi"; Enabled = true; }
+            { Path = "OpenLinuxBoot.efi"; Enabled = true; }
+            { Path = "OpenUsbKbDxe.efi"; Enabled = true; }
+          ];
+        };
       })
     ];
   };
