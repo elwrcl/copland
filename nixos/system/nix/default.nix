3f14961c51f03965e6d165742c4177faf7f3d296
elwrcl <elwerici@icloud.com>
2026-04-19 01:04:58 +0300

    removed chaotic and garnix cache
    Signed-off-by: elwrcl <elwerici@icloud.com>

--- nixos/system/nix/default.nix
+++ nixos/system/nix/default.nix
@@ -2,7 +2,10 @@
 
 {
   nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
 
     max-jobs = "auto";
     cores = 0;
@@ -11,17 +14,13 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://noctalia.cachix.org"
-      "https://nyx.chaotic.cx"
       "https://attic.xuyh0120.win/lantian"
-      "https://cache.garnix.io"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agF+ZjL1siDnBBn7gCnk="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
